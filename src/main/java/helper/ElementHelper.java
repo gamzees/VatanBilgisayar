e8f96aed90693322a9571e62112e12927b5c1ddd
YUSUF-PC\Yusuf-Testinium <yusufbuzrul@gmail.com>
2021-03-03 15:53:44 +0300

    [Edited] xpath's changed with css selector.

--- src/main/java/helper/ElementHelper.java
+++ src/main/java/helper/ElementHelper.java
@@ -6,8 +6,8 @@ import org.openqa.selenium.By;
 public class ElementHelper {
     public static By getElementInfoToBy(ElementInfo elementInfo) {
         By by = null;
-        if (elementInfo.getType().equals("className")) {
-            by = By.className(elementInfo.getValue());
+        if (elementInfo.getType().equals("css")) {
+            by = By.cssSelector(elementInfo.getValue());
         } else if (elementInfo.getType().equals("id")) {
             by = By.id(elementInfo.getValue());
         }else if (elementInfo.getType().equals("xpath")) {
