12658952053c1dd1941d9491c468a1b69dbb9d89
Gamze Taşkın <gamzeetskn@gmail.com>
2021-03-03 18:01:35 +0300

    no message

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -24,7 +24,6 @@ public class BaseTest {
         logger.info("    Test Başlatıldı    ");
         logger.info("======================================");
     }
-    //gamzeeeee
 
     @BeforeScenario
     public void beforeScenario() {
